eb60240cb1c80b0986d208b8232b3d63d60513e3
PraveenRL <rlppraveen@gmail.com>
2020-03-18 18:40:07 +0530

    foreign key, constraint

--- src/controller/table/table.controller.ts
+++ src/controller/table/table.controller.ts
@@ -3,7 +3,6 @@ import { Router, Request, Response } from "express";
 import IController from "../../interface/controller.interface";
 import { pool } from "../../queries";
 
-
 class TableController implements IController {
     public path: string = '/table';
     public router = Router();
@@ -21,6 +20,12 @@ class TableController implements IController {
         this.router.get(`${this.path}/dropBusTable`, this.dropBusTable);
         this.router.get(`${this.path}/dropTripTable`, this.dropTripTable);
         this.router.get(`${this.path}/dropBookingTable`, this.dropBookingTable);
+        this.router.get(`${this.path}/disconnectDB`, this.disconnectDB);
+
+        this.router.get(`${this.path}/createAccountTable`, this.createAccountTable);
+        this.router.get(`${this.path}/createRoleTable`, this.createRoleTable);
+        this.router.get(`${this.path}/createAccountRoleTable`, this.createAccountRoleTable);
+
     }
 
     private createUserTable = async (request: Request, response: Response) => {
@@ -148,6 +153,63 @@ class TableController implements IController {
         })
     }
 
+    private disconnectDB = async (request: Request, response: Response) => {
+        pool.on('remove', (Client) => {
+            Client.release();
+            process.exit(-1);
+        })
+        response.send('Disconnect DB')
+    }
+
+    // Create Table, Primary Key, Foreign Key, Constraint ------------------
+    private createAccountTable = (request: Request, response: Response) => {
+        let sql: string = `CREATE TABLE IF NOT EXISTS account(
+            user_id serial PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            password VARCHAR(50) NOT NULL,
+            email VARCHAR(355) NOT NULL,
+            created_on TIMESTAMP NOT NULL,
+            last_login TIMESTAMP
+        )`;
+        pool.query(sql).then(result => {
+            response.send(result);
+        }).catch(error => {
+            response.status(400).send(error);
+        })
+    }
+
+    private createRoleTable = async (request: Request, response: Response) => {
+        let sql: string = `CREATE TABLE IF NOT EXISTS role(
+            role_id serial PRIMARY KEY,
+            role_name VARCHAR (255) NOT NULL
+        )`;
+        await pool.query(sql).then(result => {
+            response.send(result);
+        }).catch(error => {
+            response.status(400).send(error);
+        })
+    }
+
+    private createAccountRoleTable = async (request: Request, response: Response) => {
+        let sql: string = `CREATE TABLE IF NOT EXISTS account_role(
+            user_id integer NOT NULL,
+            role_id integer NOT NULL,
+            grant_date TIMESTAMP without TIME ZONE,
+            PRIMARY KEY (user_id, role_id),
+            CONSTRAINT account_role_role_id_fkey FOREIGN KEY (role_id)
+                REFERENCES role (role_id) MATCH SIMPLE
+                ON UPDATE NO ACTION ON DELETE NO ACTION,
+            CONSTRAINT account_role_user_id_fkey FOREIGN KEY (user_id)
+                REFERENCES account (user_id) MATCH SIMPLE
+                ON UPDATE NO ACTION ON DELETE NO ACTION
+        );`;
+        pool.query(sql).then(result => {
+            response.send(result);
+        }).catch(error => {
+            response.status(400).send(error);
+        })
+    }
+    //---------------------------------------------------------------------------------
 
 }
 
